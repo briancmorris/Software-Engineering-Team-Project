63a39b11c713c39de4450b66881af32706068f1c
BrianMorris <briancmorris@live.com>
2018-04-07 22:07:14 -0400

    Completed unit testing of LOINCCode and LOINCCodeForm for #44.
    Static database methods still need to be tested once we store
    information on the database.

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/unit/LOINCCodeTest.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/unit/LOINCCodeTest.java
@@ -10,7 +10,7 @@ import edu.ncsu.csc.itrust2.forms.admin.LOINCCodeForm;
 import edu.ncsu.csc.itrust2.models.persistent.LOINCCode;
 
 /**
- * Tests the LOINCCode class for completion.
+ * Tests the LOINCCode and LOINCCodeForm classes for completion.
  *
  * @author Brian Morris
  *
@@ -18,10 +18,10 @@ import edu.ncsu.csc.itrust2.models.persistent.LOINCCode;
 public class LOINCCodeTest {
 
     /**
-     * Tests the constructor that uses a LOINCCodeForm.
+     * Tests the LOINCCode constructor that uses a LOINCCodeForm.
      */
     @Test
-    public void testFormConstructor () {
+    public void testLOINCCodeConstructor () {
         // Test form.
         final LOINCCodeForm testForm = new LOINCCodeForm();
         testForm.setId( new Long( 5 ) );
@@ -35,12 +35,38 @@ public class LOINCCodeTest {
         final LOINCCode test = new LOINCCode( testForm );
 
         // Test assertions.
-        assertEquals( 5, test.getId().longValue() );
-        assertTrue( "12345-6".equals( test.getCode() ) );
-        assertTrue( "This is a test lcn.".equals( test.getLongCommonName() ) );
-        assertTrue( "This is a test component.".equals( test.getComponent() ) );
-        assertTrue( "This is a test prop.".equals( test.getProp() ) );
-        assertTrue( "This is a test su.".equals( test.getSpecialUsage() ) );
+        assertEquals( testForm.getId().longValue(), test.getId().longValue() );
+        assertTrue( testForm.getCode().equals( test.getCode() ) );
+        assertTrue( testForm.getLongCommonName().equals( test.getLongCommonName() ) );
+        assertTrue( testForm.getComponent().equals( test.getComponent() ) );
+        assertTrue( testForm.getProp().equals( test.getProp() ) );
+        assertTrue( testForm.getSpecialUsage().equals( test.getSpecialUsage() ) );
+    }
+
+    /**
+     * Tests the LOINCCodeForm constructor that uses a LOINCCode.
+     */
+    @Test
+    public void testLOINCCodeFormConstructor () {
+        // Test code.
+        final LOINCCode testCode = new LOINCCode();
+        testCode.setId( new Long( 5 ) );
+        testCode.setCode( "12345-6" );
+        testCode.setLongCommonName( "This is a test lcn." );
+        testCode.setComponent( "This is a test component." );
+        testCode.setProp( "This is a test prop." );
+        testCode.setSpecialUsage( "This is a test su." );
+
+        // Test form.
+        final LOINCCodeForm test = new LOINCCodeForm( testCode );
+
+        // Test assertions.
+        assertEquals( testCode.getId().longValue(), test.getId().longValue() );
+        assertTrue( testCode.getCode().equals( test.getCode() ) );
+        assertTrue( testCode.getLongCommonName().equals( test.getLongCommonName() ) );
+        assertTrue( testCode.getComponent().equals( test.getComponent() ) );
+        assertTrue( testCode.getProp().equals( test.getProp() ) );
+        assertTrue( testCode.getSpecialUsage().equals( test.getSpecialUsage() ) );
     }
 
     /**
@@ -49,14 +75,19 @@ public class LOINCCodeTest {
     @Test
     public void testId () {
 
-        // Test code.
-        final LOINCCode test = new LOINCCode();
+        // Test code and form.
+        final LOINCCode testCode = new LOINCCode();
+        final LOINCCodeForm testForm = new LOINCCodeForm();
+        final Long testId = new Long( 5 );
 
         // Assert the the initial ID is null.
-        assertNull( test.getId() );
-        test.setId( new Long( 5 ) );
+        assertNull( testCode.getId() );
+        assertNull( testForm.getId() );
+        testCode.setId( testId );
+        testForm.setId( testId );
         // Assert that set/get work as intended.
-        assertEquals( 5, test.getId().longValue() );
+        assertEquals( testId.longValue(), testCode.getId().longValue() );
+        assertEquals( testId.longValue(), testForm.getId().longValue() );
 
     }
 
@@ -66,14 +97,19 @@ public class LOINCCodeTest {
     @Test
     public void testCode () {
 
-        // Test code.
-        final LOINCCode test = new LOINCCode();
+        // Test code and form.
+        final LOINCCode testCode = new LOINCCode();
+        final LOINCCodeForm testForm = new LOINCCodeForm();
+        final String testCodeStr = "12345-6";
 
         // Assert the the initial code is null.
-        assertNull( test.getCode() );
+        assertNull( testCode.getCode() );
+        assertNull( testForm.getCode() );
         // Assert that set/get work as intended.
-        test.setCode( "12345-6" );
-        assertTrue( "12345-6".equals( test.getCode() ) );
+        testCode.setCode( testCodeStr );
+        testForm.setCode( testCodeStr );
+        assertTrue( testCodeStr.equals( testCode.getCode() ) );
+        assertTrue( testCodeStr.equalsIgnoreCase( testForm.getCode() ) );
 
     }
 
@@ -83,14 +119,19 @@ public class LOINCCodeTest {
     @Test
     public void testLongCommonName () {
 
-        // Test code.
-        final LOINCCode test = new LOINCCode();
+        // Test code and form.
+        final LOINCCode testCode = new LOINCCode();
+        final LOINCCodeForm testForm = new LOINCCodeForm();
+        final String testLongCommonName = "This is a test.";
 
         // Assert the the initial long common name is null.
-        assertNull( test.getLongCommonName() );
+        assertNull( testCode.getLongCommonName() );
+        assertNull( testForm.getLongCommonName() );
         // Assert that set/get work as intended.
-        test.setLongCommonName( "This is a test." );
-        assertTrue( "This is a test.".equals( test.getLongCommonName() ) );
+        testCode.setLongCommonName( testLongCommonName );
+        testForm.setLongCommonName( testLongCommonName );
+        assertTrue( testLongCommonName.equals( testCode.getLongCommonName() ) );
+        assertTrue( testLongCommonName.equals( testForm.getLongCommonName() ) );
 
     }
 
@@ -100,14 +141,19 @@ public class LOINCCodeTest {
     @Test
     public void testSpecialUsage () {
 
-        // Test code.
-        final LOINCCode test = new LOINCCode();
+        // Test code and form.
+        final LOINCCode testCode = new LOINCCode();
+        final LOINCCodeForm testForm = new LOINCCodeForm();
+        final String testSpecialUsage = "This is a test.";
 
-        // Assert the the initial special usage is null.
-        assertNull( test.getSpecialUsage() );
+        // Assert the the initial long common name is null.
+        assertNull( testCode.getSpecialUsage() );
+        assertNull( testForm.getSpecialUsage() );
         // Assert that set/get work as intended.
-        test.setSpecialUsage( "This is a test." );
-        assertTrue( "This is a test.".equals( test.getSpecialUsage() ) );
+        testCode.setSpecialUsage( testSpecialUsage );
+        testForm.setSpecialUsage( testSpecialUsage );
+        assertTrue( testSpecialUsage.equals( testCode.getSpecialUsage() ) );
+        assertTrue( testSpecialUsage.equals( testForm.getSpecialUsage() ) );
 
     }
 
@@ -117,14 +163,19 @@ public class LOINCCodeTest {
     @Test
     public void testComponent () {
 
-        // Test code.
-        final LOINCCode test = new LOINCCode();
+        // Test code and form.
+        final LOINCCode testCode = new LOINCCode();
+        final LOINCCodeForm testForm = new LOINCCodeForm();
+        final String testComponent = "This is a test.";
 
-        // Assert the the initial component is null.
-        assertNull( test.getComponent() );
+        // Assert the the initial long common name is null.
+        assertNull( testCode.getComponent() );
+        assertNull( testForm.getComponent() );
         // Assert that set/get work as intended.
-        test.setComponent( "This is a test." );
-        assertTrue( "This is a test.".equals( test.getComponent() ) );
+        testCode.setComponent( testComponent );
+        testForm.setComponent( testComponent );
+        assertTrue( testComponent.equals( testCode.getComponent() ) );
+        assertTrue( testComponent.equals( testForm.getComponent() ) );
 
     }
 
@@ -134,14 +185,19 @@ public class LOINCCodeTest {
     @Test
     public void testProp () {
 
-        // Test code.
-        final LOINCCode test = new LOINCCode();
+        // Test code and form.
+        final LOINCCode testCode = new LOINCCode();
+        final LOINCCodeForm testForm = new LOINCCodeForm();
+        final String testProp = "This is a test.";
 
-        // Assert the the initial prop is null.
-        assertNull( test.getProp() );
+        // Assert the the initial long common name is null.
+        assertNull( testCode.getProp() );
+        assertNull( testForm.getProp() );
         // Assert that set/get work as intended.
-        test.setProp( "This is a test." );
-        assertTrue( "This is a test.".equals( test.getProp() ) );
+        testCode.setProp( testProp );
+        testForm.setProp( testProp );
+        assertTrue( testProp.equals( testCode.getProp() ) );
+        assertTrue( testProp.equals( testForm.getProp() ) );
 
     }
 }
