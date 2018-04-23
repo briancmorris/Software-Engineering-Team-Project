91989c5fe68b08ad1a49c5fd9953978486c7542f
Anuraag <Anuraag@192.168.0.8>
2018-04-23 02:33:32 -0400

    finished cucumber testing for Personal Reps and added enhancements (confirmation messages and minor UI changes) to Personal Reps

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/PersonalRepsStepDefs.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/PersonalRepsStepDefs.java
@@ -11,7 +11,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.After;
@@ -22,6 +23,7 @@ import cucumber.api.java.en.When;
 import edu.ncsu.csc.itrust2.models.enums.State;
 import edu.ncsu.csc.itrust2.models.persistent.Patient;
 import edu.ncsu.csc.itrust2.models.persistent.User;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public class PersonalRepsStepDefs {
 
@@ -33,8 +35,13 @@ public class PersonalRepsStepDefs {
     @Before
     public void setup () {
 
-        driver = new HtmlUnitDriver( true );
-        wait = new WebDriverWait( driver, 5 );
+        // driver = new HtmlUnitDriver( true );
+        // wait = new WebDriverWait( driver, 5 );
+        ChromeDriverManager.getInstance().setup();
+        final ChromeOptions options = new ChromeOptions();
+        options.addArguments( "window-size=1200x600" );
+        options.addArguments( "blink-settings=imagesEnabled=false" );
+        driver = new ChromeDriver( options );
     }
 
     @After
@@ -42,7 +49,7 @@ public class PersonalRepsStepDefs {
         driver.close();
     }
 
-    @Given ( "A patient exists with the name: (.+) and DOB: (.+)" )
+    @Given ( "A patient exists with name: (.+) and DOB: (.+)" )
     public void patientExists ( final String name, final String birthday ) throws ParseException {
 
         /* Create patient record */
@@ -69,7 +76,7 @@ public class PersonalRepsStepDefs {
 
     }
 
-    @When ( "I log into iTrust2 as a Patient" )
+    @When ( "I log into iTrust2 as sample Patient" )
     public void patientLogin () {
         driver.get( baseUrl );
         final WebElement username = driver.findElement( By.name( "username" ) );
@@ -90,26 +97,83 @@ public class PersonalRepsStepDefs {
 
     @When ( "I add a patient by username: (.+) as a representative of mine" )
     public void fillRepresentative ( final String username ) {
-        final WebElement firstName = driver.findElement( By.name( "addRepForm" ) );
+        final WebElement firstName = driver.findElement( By.name( "rep" ) );
         firstName.clear();
         firstName.sendKeys( username );
 
-        final WebElement submit = driver.findElement( By.className( "btn" ) );
-        submit.click();
+        driver.findElement( By.name( "submit" ) ).click();
     }
 
     @Then ( "The representative: (.+) is successfully added" )
-    public void viewRepresentative ( final String username ) {
-        final WebElement rep = driver.findElement( By.name( "idCell" ) );
-        assertEquals( rep.getAttribute( "value" ), username );
+    public void viewRepresentative ( final String username ) throws InterruptedException {
+        Thread.sleep( 2000 );
+        final WebElement message = driver.findElement( By.id( "succP" ) );
+        assertEquals( "Successfully added personal representative", message.getText() );
+
+        final WebElement name = driver.findElement(
+                By.xpath( "//*[@id=\"wrap\"]/div[1]/div/div[1]/table/tbody/tr/td[1]/table/tbody/tr[2]/td[1]" ) );
+        assertEquals( username, name.getText() );
     }
 
-    @When ( "Added representative logs into into iTrust2 as Patient" )
-    public void patientLogin2 () {
+    // @When ( "Added representative logs into into iTrust2 as Patient" )
+    // public void repLogin () {
+    // driver.get( baseUrl );
+    // setTextField( By.name( "username" ), "AliceThirteen" );
+    // setTextField( By.name( "password" ), "123456" );
+    // driver.findElement( By.className( "btn" ) ).click();
+    // }
+    //
+    // @When ( "Representative navigates to the Edit Personal Representatives
+    // page" )
+    // public void viewAddedRepresentatives () {
+    // ( (JavascriptExecutor) driver )
+    // .executeScript(
+    // "document.getElementById('editpersonalrepresentatives-patient').click();"
+    // );
+    // }
+    //
+    // @Then ( "Representative is presented with a patient in the list of
+    // patients he represents" )
+    // public void viewRepresentative2 () {
+    // final WebElement rep = driver.findElement( By.name( "idCell" ) );
+    // assertEquals( rep.getAttribute( "value" ), "patient" );
+    // }
+
+    // new scenario
+
+    @Given ( "A rep exists with name: (.+) and DOB: (.+)" )
+    public void repExists ( final String name, final String birthday ) throws ParseException {
+
+        /* Create patient record */
+
+        final Patient patient = new Patient();
+        patient.setSelf( User.getByName( "BobTheFourYearOld" ) );
+
+        patient.setFirstName( name.split( " " )[0] );
+        patient.setLastName( name.split( " " )[1] );
+        patient.setEmail( "email2@mail.com" );
+        patient.setAddress1( "846 address place" );
+        patient.setCity( "citytowntwo" );
+        patient.setState( State.CA );
+        patient.setZip( "91506" );
+        patient.setPhone( "123-456-7891" );
+        final SimpleDateFormat sdf = new SimpleDateFormat( "MM/DD/YYYY", Locale.ENGLISH );
+
+        final Calendar time = Calendar.getInstance();
+        time.setTime( sdf.parse( birthday ) );
+
+        patient.setDateOfBirth( time );
+
+        patient.save();
+
+    }
+
+    @When ( "I log into iTrust2 again as sample HCP" )
+    public void hcpLogin () {
         driver.get( baseUrl );
         final WebElement username = driver.findElement( By.name( "username" ) );
         username.clear();
-        username.sendKeys( "AliceThirteen" );
+        username.sendKeys( "hcp" );
         final WebElement password = driver.findElement( By.name( "password" ) );
         password.clear();
         password.sendKeys( "123456" );
@@ -117,16 +181,83 @@ public class PersonalRepsStepDefs {
         submit.click();
     }
 
-    @When ( "Representative navigates to the Edit Personal Representatives page" )
-    public void viewAddedRepresentatives () {
+    @When ( "I view the HCP Edit Personal Representatives page" )
+    public void editRepresentativesHCP () {
+        ( (JavascriptExecutor) driver )
+                .executeScript( "document.getElementById('editpersonalrepresentatives-hcp').click();" );
+    }
+
+    @When ( "I view representatives for: (.+)" )
+    public void viewPatientRepresentatives ( final String username ) {
+        final WebElement firstName = driver.findElement( By.name( "patient" ) );
+        firstName.clear();
+        firstName.sendKeys( username );
+
+        driver.findElement( By.name( "submit" ) ).click();
+    }
+
+    @Then ( "I am presented with (.+) in the list of representatives" )
+    public void viewReps ( final String rep ) throws InterruptedException {
+        Thread.sleep( 2000 );
+        final WebElement name = driver.findElement(
+                By.xpath( "//*[@id=\"wrap\"]/div[1]/div/div[1]/table/tbody/tr/td[1]/table/tbody/tr[2]/td" ) );
+        assertEquals( rep, name.getText() );
+    }
+
+    @When ( "I add another representative: (.+)" )
+    public void addRep ( final String rep ) throws InterruptedException {
+        final WebElement firstName = driver.findElement( By.name( "rep" ) );
+        firstName.clear();
+        firstName.sendKeys( rep );
+
+        driver.findElement( By.name( "submitRep" ) ).click();
+    }
+
+    @Then ( "I am presented with another (.+) added to the list of representatives" )
+    public void viewRepsAgain ( final String rep ) throws InterruptedException {
+        Thread.sleep( 2000 );
+        final WebElement message = driver.findElement( By.id( "succP" ) );
+        assertEquals( "Successfully added personal representative", message.getText() );
+    }
+
+    // new scenario
+
+    @When ( "I log into iTrust2 again as sample Patient" )
+    public void patientLogin2 () {
+        driver.get( baseUrl );
+        final WebElement username = driver.findElement( By.name( "username" ) );
+        username.clear();
+        username.sendKeys( "patient" );
+        final WebElement password = driver.findElement( By.name( "password" ) );
+        password.clear();
+        password.sendKeys( "123456" );
+        final WebElement submit = driver.findElement( By.className( "btn" ) );
+        submit.click();
+    }
+
+    @When ( "I view Personal Representatives page" )
+    public void viewPersonalRepresentatives () {
         ( (JavascriptExecutor) driver )
                 .executeScript( "document.getElementById('editpersonalrepresentatives-patient').click();" );
     }
 
-    @Then ( "Representative is presented with a patient in the list of patients he represents" )
-    public void viewRepresentative2 () {
-        final WebElement rep = driver.findElement( By.name( "idCell" ) );
-        assertEquals( rep.getAttribute( "value" ), "patient" );
+    @When ( "I undeclare previously added patient to not be a representative" )
+    public void undeclareRep () throws InterruptedException {
+        Thread.sleep( 5000 );
+        final WebElement button = driver.findElement(
+                By.xpath( "//*[@id=\"wrap\"]/div[1]/div/div[1]/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/input" ) );
+        button.click();
+        Thread.sleep( 5000 );
+        final WebElement button2 = driver.findElement(
+                By.xpath( "//*[@id=\"wrap\"]/div[1]/div/div[1]/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/input" ) );
+        button2.click();
+    }
+
+    @Then ( "I am presented with an empty list of representatives" )
+    public void checkReps () throws InterruptedException {
+        Thread.sleep( 2000 );
+        final WebElement message = driver.findElement( By.id( "messageView" ) );
+        assertEquals( "NO PERSONAL REPRESENTATIVES", message.getText() );
     }
 
 }
